64a7d3350f939684b9eb30645b9d0067e18665a1
Jared Rhodes <jaredrhodes15@gmail.com>
2019-01-12 06:53:55 -0700

    firebase initialized

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+$(document).ready(function () {
+
 var tName = $("name");
 var tDestination = $("destination");
 var tTime = $("time");
 var tFrequency = $("frequency");
 
 
+var database = firebase.database();
+
+
+
+});
